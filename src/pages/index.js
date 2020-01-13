7fc6803abecaec014f9ccd84853d6b8bce9137c3
michelneeser <github@michelneeser.ch>
2020-01-14 00:21:44 +0100

    seriously started working on it ;-)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React from 'react';
 
 import styles from './index.module.scss';
-import Layout from "../components/layout/layout";
+import portraitImage from '../images/portrait.jpg';
 
 const IndexPage = () => (
-  <Layout>
-    <div className={styles.titleWrapper}>
-      <div className={styles.image}></div>
-      <h1 className={styles.title}>Michel Neeser</h1>
-      <h2 className={styles.subtitle}>Full Stack Software Developer</h2>
+  <div>
+    <div className={styles.box}>
+      <img className={styles.portrait} src={portraitImage} />
+      <h1>Michel Neeser</h1>
+      <h2>Full Stack Software Developer</h2>
+      <div className={styles.contentBox}>
+        <h3>Skills</h3>
+      </div>
+      <div className={styles.contentBox}>
+        <h3>Projects</h3>
+      </div>
+      <div className={styles.contentBox}>
+        <h3>Contact</h3>
+      </div>
     </div>
-  </Layout >
-)
+  </div >
+);
 
 export default IndexPage;

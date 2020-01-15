f0ecaf842fb5b0ae58d981d2ffc39222a2bac729
michelneeser <github@michelneeser.ch>
2020-01-15 22:31:17 +0100

    clean-up

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -9,31 +9,31 @@ const Skills = () => {
       <Section title="Skills / Frontend">
         <div className={styles.section}>
           <div className={styles.iconContainer}>
-            <i class="devicon-css3-plain"></i>
+            <i className="devicon-css3-plain"></i>
             <div>CSS3</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-html5-plain"></i>
+            <i className="devicon-html5-plain"></i>
             <div>HTML5</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-javascript-plain"></i>
+            <i className="devicon-javascript-plain"></i>
             <div>JavaScript</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-react-plain"></i>
+            <i className="devicon-react-plain"></i>
             <div>React</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-sass-original"></i>
+            <i className="devicon-sass-original"></i>
             <div>Sass</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-bootstrap-plain"></i>
+            <i className="devicon-bootstrap-plain"></i>
             <div>Bootstrap</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-jquery-plain"></i>
+            <i className="devicon-jquery-plain"></i>
             <div>jQuery</div>
           </div>
         </div>
@@ -41,27 +41,27 @@ const Skills = () => {
       <Section title="Skills / Backend">
         <div className={styles.section}>
           <div className={styles.iconContainer}>
-            <i class="devicon-nodejs-plain"></i>
+            <i className="devicon-nodejs-plain"></i>
             <div>Node.js</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-express-original"></i>
+            <i className="devicon-express-original"></i>
             <div>Express</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-mysql-plain"></i>
+            <i className="devicon-mysql-plain"></i>
             <div>MySQL</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-mongodb-plain"></i>
+            <i className="devicon-mongodb-plain"></i>
             <div>MongoDB</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-java-plain"></i>
+            <i className="devicon-java-plain"></i>
             <div>Java</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-tomcat-line"></i>
+            <i className="devicon-tomcat-line"></i>
             <div>Tomcat</div>
           </div>
         </div>
@@ -69,19 +69,19 @@ const Skills = () => {
       <Section title="Skills / Tools">
         <div className={styles.section}>
           <div className={styles.iconContainer}>
-            <i class="devicon-git-plain"></i>
+            <i className="devicon-git-plain"></i>
             <div>Git</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-github-original"></i>
+            <i className="devicon-github-original"></i>
             <div>GitHub</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-heroku-original"></i>
+            <i className="devicon-heroku-original"></i>
             <div>Heroku</div>
           </div>
           <div className={styles.iconContainer}>
-            <i class="devicon-confluence-plain"></i>
+            <i className="devicon-confluence-plain"></i>
             <div>Confluence</div>
           </div>
         </div>

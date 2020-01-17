44c10cb241a8ce9d0b39b734eca9c596e82396d1
michelneeser <github@michelneeser.ch>
2020-01-17 23:22:16 +0100

    styling of about and experiences

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,17 +6,9 @@ import styles from './about.module.scss';
 const About = () => {
   return (
     <div>
-      <Section title="Nice to meet you! 🎉">
+      <Section title="Nice to meet you!&nbsp;&nbsp;🎉">
         <div className={styles.content}>
-          <div>
-            <p>Well, I didn't feel like writing an entire paragraph about me. But to avoid having only Lorem Ipsum here, a few key points about me follow:</p>
-            <ul>
-              <li>I am <span className={styles.highlight}>passionate about software engineering</span> and computer stuff in general since I can remember.</li>
-              <li>I love learning new things, especially in software and <span className={styles.highlight}>web development.</span></li>
-              <li>I am a <span className={styles.highlight}>professional software developer for more than 10 years,</span> currently employed in a web agency as technical lead.</li>
-              <li>I work on Java web backends since 2014, but my interest has recently turned to <span className={styles.highlight}>full stack development</span> with all its glory and challenges.</li>
-            </ul>
-          </div>
+          <p>I am <span className={styles.highlight}>passionate about software engineering</span> and computer stuff in general since I can remember. I love learning and exploring new things, especially in software and <span className={styles.highlight}>web development.</span> By day, I am a professional software developer <span className={styles.highlight}>for more than 10 years</span> now, currently employed in a web agency as technical lead. I work on Java web backends since 2014, but my interest has recently turned to full stack web development with all its glory and challenges. So by night, you can find me hacking away on the web. ⚡️🖥</p>
         </div>
       </Section>
     </div >
--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+import { faLaptopCode, faMapPin } from '@fortawesome/free-solid-svg-icons';
 
 import Section from '../section/Section';
 import styles from './experience.module.scss';
@@ -10,20 +10,80 @@ const Experience = () => {
     <div>
       <Section title="Professional Experience">
         <div className={styles.jobs}>
-          <div className={styles.title}>
-            <div>
-              <span>Technical Lead @ MySign AG</span>
-              <FontAwesomeIcon
-                icon={faLaptopCode}
-                className={styles.icon} />
+          <div className={styles.job}>
+            <div className={styles.title}>
+              <div>
+                <span>Technical Lead @ MySign AG</span>
+                <span>
+                  <FontAwesomeIcon
+                    icon={faLaptopCode}
+                    className={styles.icon} />
+                </span>
+              </div>
+            </div>
+            <div className={styles.accountabilities}>
+              <ul>
+                <li>Leading the technical development and planning of flagship web projects.</li>
+                <li>Conception of data exchange interfaces to third party systems of customers.</li>
+                <li>Responsible the vocational training of software developer trainees.</li>
+              </ul>
+            </div>
+            <div className={styles.date}>
+              <span>⏰ October 2018 &ndash; today</span>
+              <span>|</span>
+              <span>📍 Olten, Switzerland</span>
             </div>
           </div>
-          <div className={styles.accountabilities}>
-            <ul>
-              <li>Some stuff</li>
-              <li>Some other stuff</li>
-              <li>Some random stuff</li>
-            </ul>
+
+          <div className={styles.job}>
+            <div className={styles.title}>
+              <div>
+                <span>Java Developer @ MySign AG</span>
+                <span>
+                  <FontAwesomeIcon
+                    icon={faLaptopCode}
+                    className={styles.icon} />
+                </span>
+              </div>
+            </div>
+            <div className={styles.accountabilities}>
+              <ul>
+                <li>Technical conception and development of MySign products and customer projects.</li>
+                <li>Principal software developer of some of the flagship web projects of MySign.</li>
+                <li>Vocational training of software developer trainees.</li>
+              </ul>
+            </div>
+            <div className={styles.date}>
+              <span>⏰ April 2014 &ndash; October 2018</span>
+              <span>|</span>
+              <span>📍 Olten, Switzerland</span>
+            </div>
+          </div>
+
+          <div className={styles.job}>
+            <div className={styles.title}>
+              <div>
+                <span>Java Software Engineer @ Finnova AG</span>
+                <span>
+                  <FontAwesomeIcon
+                    icon={faLaptopCode}
+                    className={styles.icon} />
+                </span>
+              </div>
+            </div>
+            <div className={styles.accountabilities}>
+              <ul>
+                <li>Development of core functionalities.</li>
+                <li>Redesign of the central operating component for processing bank orders.</li>
+                <li>Maintenance for central basic components in the core system and UI framework.</li>
+                <li>Framework support for software developers.</li>
+              </ul>
+            </div>
+            <div className={styles.date}>
+              <span>⏰ August 2013 &ndash; April 2014</span>
+              <span>|</span>
+              <span>📍 Lenzburg, Switzerland</span>
+            </div>
           </div>
         </div>
       </Section>
